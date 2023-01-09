aaf3d9fb8e0a7642086c2dcb86649eb4a96b5636
Simon Cleriot <cleriotsimon@gmail.com>
2023-01-09 18:00:30 +0100

    Works with hybridpicker

--- src/PDF/PDFOptions.ts
+++ src/PDF/PDFOptions.ts
@@ -26,16 +26,46 @@ export default class PDFOptionsModule extends SlideOptionsModule {
       fileType.value = 'google'
     }
 
+    const legacyDefaultType = update.option("remoteFiles").modelValue ? "google" : "media"
+    const filetype = ref(update.option("type").modelValue || legacyDefaultType);
+    const account = ref(null)
+
+    const updateMediaId = (value: number | number[]) => {
+      value = Array.isArray(value) ? value : [value]
+      if (filetype.value === "media") {
+        update.option("media")["onUpdate:modelValue"](value)
+        update.option("remoteFiles")["onUpdate:modelValue"](null);
+        update.option("type")["onUpdate:modelValue"]("media");
+        update.option("__accounts")["onUpdate:modelValue"]([]);
+      } else {
+        update.option("remoteFiles")["onUpdate:modelValue"](value);
+        update.option("media")["onUpdate:modelValue"](null)
+        update.option("type")["onUpdate:modelValue"](filetype.value);
+        update.option("__accounts")["onUpdate:modelValue"]([account.value]);
+      }
+    }
+
+    const modelValue = computed(() => {
+      return update.option(fileType.value == "media" ? 'media' : 'remoteFiles').modelValue
+    })
+
     let hybrid_options = computed(() => {
-      let options: any = { type: 'pdfs', filetype: fileType.value, 'onUpdate:filetype': (value) => fileType.value = value }
+      let options: any = {
+        type: 'pdfs',
+        account_id: account.value,
+        'onUpdate:account_id': (val) => account.value = val,
+        filetype: fileType.value,
+        'onUpdate:filetype': (value) => fileType.value = value,
+        modelValue: modelValue.value,
+        'onUpdate:modelValue': (value) => updateMediaId(value),
+      }
       if (fileType.value == 'media') {
-        options = { ...options, ...update.option('media'), onSelectedMedias: (medias: Array<any>) => {
+        options = { ...options, onSelectedMedias: (medias: Array<any>) => {
           context.updateAutoName(medias[0]?.filename)
         } }
       } else {
         options = {
           ...options,
-          ...update.option('remoteFiles'),
           noAccountsSelect: true,
           onSelectedMedias: (medias: Array<any>) => {
             context.updateAutoName(medias[0]?.filename)
